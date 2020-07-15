a7046faabb33c383096cb61a6d46fb8b2ab5236e
Paul Sud <paulsud@stanford.edu>
2020-07-15 13:01:14 -0700

    update logging for gunicorn

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+import logging
+
 from gcs_s3_transfer_service import app
 
 if __name__ == "__main__":
     # This is used when running locally only. On Google App Engine, a production-grade
     # WSGI server will be used automatically.
     app.run(host="127.0.0.1", port=8080, debug=True)
+else:
+    # When not running in debug mode need to set the app logging handlers
+    gunicorn_logger = logging.getLogger("gunicorn.error")
+    app.logger.handlers.extend(gunicorn_logger.handlers)
+    app.logger.setLevel(gunicorn_logger.level)
